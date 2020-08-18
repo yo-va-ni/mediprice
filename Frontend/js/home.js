2eda26276c3b8ac2e81407332e79aac9b9c7eb99
Yovani <yovani.pahuara.1596@gmail.com>
2020-08-18 12:14:16 -0500

    Busqueda: listo

--- Frontend/js/home.js
+++ Frontend/js/home.js
@@ -12,14 +12,16 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const auth = firebase.auth();
-const fs = firebase.firestore();
+let database = firebase.firestore();
 
 let items = [];
+let medicamentos = [];
 
 const logout = document.getElementById("logout");
-const link_mapa_detalle = document.getElementsByClassName("link-mapa");
+let link_mapa_detalle = [];
 const columns_filter = document.getElementById("data-filter-items");
-items.push(document.getElementById("r-111111"));
+
+// Verificacion de inicio de sesion
 
 auth.onAuthStateChanged((user) => {
     if (user) {
@@ -30,6 +32,7 @@ auth.onAuthStateChanged((user) => {
   }
 );
 
+// Finalizacion de sesion
 logout.addEventListener("click", (ev) => {
     ev.preventDefault();
     auth.signOut().then(() => {
@@ -39,8 +42,58 @@ logout.addEventListener("click", (ev) => {
   });
 
 
-items.forEach(element => {
-    element.addEventListener("click", (ev) =>{
+
+
+// Ejecución de busqueda
+let form_busqueda = document.getElementById("busqueda-medicamento");
+form_busqueda.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    let formData = new FormData(form_busqueda);
+    let producto = formData.entries().next().value[1];
+    productos = database.collection("medicamentos")
+    .where("nombreComercial", "==", producto)
+    .get()
+    .then(result => {
+        
+        let card = document.createElement("div");
+        let accordion = document.getElementById("accordionData");
+        accordion.innerHTML = ""
+        card.className = "card";
+        result.forEach(med => {
+            medicamentos.push(med);
+        });
+        card.appendChild(armarBarra(medicamentos[0]));
+        card.appendChild(pintarRsultados(medicamentos));
+
+        accordion.appendChild(card);
+    });
+});
+
+// Armando la barra de presentacion
+const armarBarra = (producto) => {
+
+    let bar = document.createElement("div")
+    bar.className = "card-header card-custom";
+    let bar_name = document.createElement("h2")
+    bar_name.classList.add("mb-0");
+    bar_name.innerHTML = `<button class="btn btn-block text-left" id="r-${producto.id}" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">${producto.data().nombreComercial}</button>`;
+    let bar_mapa = document.createElement("div")
+    bar_mapa.classList.add("link-mapa");
+    bar_mapa.innerHTML = `<a href="#">VER MAPA</a>`;
+    bar_mapa.addEventListener("click", ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        let son = ev.target;
+        let id_item =son.parentElement.previousElementSibling.lastElementChild.id.slice(2)
+        console.log("redirigiendo");
+        window.location = `mapa_detalle.html?id=${id_item}`
+    });
+
+    bar.appendChild(bar_name);
+    bar.appendChild(bar_mapa);
+
+    bar.addEventListener("click", (ev) =>{
+        ev.stopPropagation();
         let id_item = ev.target.id.slice(2);
         let collapse = document.getElementById(id_item);
         if(collapse.classList.contains("show")){
@@ -49,40 +102,75 @@ items.forEach(element => {
             collapse.classList.add("show");
         }
     });
-});
+
+    return bar;
+};
+
+// Armando la cabecera de la tabla
+const armarCabecera = () => {
+    let table = document.createElement("table");
+    table.className = "table table-custom";
+    table.innerHTML = `<thead class="thead-dark">
+        <tr>
+        <th scope="col" class="col_1">Nombre</th>
+        <th scope="col" class="col_2">Concntracion</th>
+        <th scope="col" class="col_3">Presentacion</th>
+        <th scope="col" class="col_4">Fabricante</th>
+        <th scope="col" class="col_5">Reg. Sanitario</th>
+        <th scope="col" class="col_6">Sucursal</th>
+        <th scope="col" class="col_7">Precio</th>
+        </tr>
+    </thead>`;
+    return table;
+}
+
+const armarTBody = (medicamentos) => {
+    let table_body = document.createElement("tbody");
+    medicamentos.forEach(med => {
+        let data = med.data();
+        table_body.innerHTML += `<tr>
+            <td  class="col_1">${data.nombreComercial}</td>
+            <td  class="col_2">${data.concentracion}</td>
+            <td  class="col_3">Pendiente</td>
+            <td  class="col_4">${data.rucLaboratorio}</td>
+            <td  class="col_5">${med.id}</td>
+            <td  class="col_6">Pendiente</td>
+            <td  class="col_7">Pendiente</td>
+        </tr>`;
+    });
+    return table_body;
+};
+// Renderizacion de los resultados de la busqueda
+const pintarRsultados = (medicamentos) => {
+    let div_body = document.createElement("div");
+    let div_content = document.createElement("div");
+    div_body.className = "collapse";
+    div_body.id = medicamentos[0].id;
+    div_content.className = "data-details";
+    let table = armarCabecera();
+    table.appendChild(armarTBody(medicamentos));
+    
+    div_content.appendChild(table);
+    div_body.appendChild(div_content);
+    return div_body
+};
 
 
+// Agregar o quitar listas
 columns_filter.addEventListener("change", (ev) => {
     let column_id = ev.target.id;
     let item_col_filter = document.getElementById(column_id);
     let columns = document.getElementsByClassName(column_id);
-    console.log(item_col_filter.checked);
 
     if (item_col_filter.checked) {
         for (let i = 0; i < columns.length; i++) {
-            console.log("adding column");
             columns[i].style.display = "table-cell";
         }
     } else {
         for (let i = 0; i < columns.length; i++) {
-            console.log("adding column");
             columns[i].style.display = "none";
         }
     }
     
 });
-
-
-
-for (let i = 0; i < link_mapa_detalle.length; i++) {
-    const element = link_mapa_detalle[i];
-    element.addEventListener("click", ev => {
-        ev.preventDefault();
-        let son = ev.target;
-        let id_item =son.parentElement.previousElementSibling.lastElementChild.id.slice(2)
-        console.log(son.parentElement.previousElementSibling.lastElementChild.id.slice(2));
-        window.location = `mapa_detalle.html?id=${id_item}`
-    });
-}
-
 
--- Frontend/js/mapa.js
+++ Frontend/js/mapa.js
@@ -12,7 +12,7 @@ var firebaseConfig = {
     messagingSenderId: "837530993946",
     appId: "1:837530993946:web:635e4c62e3783603f818a3",
     measurementId: "G-4S2YBVF649"
-    };
+};
     // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
@@ -46,11 +46,48 @@ logout.addEventListener("click", (ev) => {
 
 */
 
-/* TODO: Poner en el mapa las ubicaciones de los demás locales
+// TODO: Poner en el mapa las ubicaciones de los demás locales
+let currentPosition = [];
+let mymap;
+let circle_distance;
 
-  ...
+// Eleccion del radio de distancia
+function changeDistance(radius){
+  circle_distance.setRadius(radius);
+};
 
-*/
+// Añadir markers de los establecimientos
+
+
+// Inicializar el mapa
+const showPosition = (position) => {
+  currentPosition = [position.coords.latitude, position.coords.longitude];
+  
+  mymap = L.map('busqueda-mapa').setView(currentPosition, 15);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  }).addTo(mymap);
+  L.marker(currentPosition).addTo(mymap);
+  circle_distance = L.circle(currentPosition, {
+    color: 'green',
+    fillColor: '#2feb7d',
+    fillOpacity: 0.3,
+    radius: document.getElementById("range-distance").value
+  }).addTo(mymap);
+};
+
+const getLocation = async () => {
+  if (navigator.geolocation) {
+    await navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("No position");
+  }
+};
+
+getLocation();
+
+
+
 
 /* TODO: Almacenar la valoracion
 
